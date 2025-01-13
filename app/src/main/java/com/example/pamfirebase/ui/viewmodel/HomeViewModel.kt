4bff3d0947714b4f43fd207496b766582c329fab
ichsanHPM <ichsanhpm@gmail.com>
2025-01-13 11:11:55 +0700

    Membuat HomeViewModel dan menambahkan import pada InsertViewModel

--- app/src/main/java/com/example/pamfirebase/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pamfirebase/ui/viewmodel/HomeViewModel.kt
@@ -2,6 +2,7 @@ package com.example.pamfirebase.ui.viewmodel
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pamfirebase.model.Mahasiswa
