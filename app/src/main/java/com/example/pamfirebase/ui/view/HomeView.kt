13ca8ec0f07af5dd0ace0aaa0c3177ac1d57f185
ichsanHPM <ichsanhpm@gmail.com>
2025-01-13 12:04:46 +0700

    Menambahkan import

--- app/src/main/java/com/example/pamfirebase/ui/view/HomeView.kt
+++ app/src/main/java/com/example/pamfirebase/ui/view/HomeView.kt
@@ -1,4 +1,4 @@
-package com.example.pamfirebase.ui.pages
+package com.example.pamfirebase.ui.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.AlertDialog
@@ -27,8 +26,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
